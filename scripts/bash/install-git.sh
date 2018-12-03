346e9660e4ae1040197626d5cb8e873c3517bbaa
Chenghung Yeh <chenghungyeh@Chenghungs-MacBook-Pro.local>
2018-12-03 13:34:32 -0800

    git

---
+++ scripts/bash/install-git.sh
@@ -0,0 +1,38 @@
+######################################
+# INSTALL OPENCV ON UBUNTU OR DEBIAN #
+######################################
+
+# |          THIS SCRIPT IS TESTED CORRECTLY ON          |
+# |------------------------------------------------------|
+# | OS               | OpenCV       | Test | Last test   |
+# |------------------|--------------|------|-------------|
+# | Ubuntu 18.04 LTS | OpenCV 3.4.2 | OK   | 18 Jul 2018 |
+
+
+
+# 1. KEEP UBUNTU OR DEBIAN UP TO DATE
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+
+
+# 2. INSTALL THE DEPENDENCIES
+
+# Build tools:
+sudo apt-get install -y build-essential cmake
+
+# GUI (if you want to use GTK instead of Qt, replace 'qt5-default' with 'libgtkglext1-dev' and remove '-DWITH_QT=ON' option in CMake):
+sudo apt-get install -y qt5-default libvtk6-dev
+
+
+
+# INSTALL THE git
+sudo apt install git-all -y
+
+
+
+# To complete this step, please visit 'http://milq.github.io/install-opencv-ubuntu-debian'.
+sudo apt-get install tmux
+sudo apt-get install htop -y
